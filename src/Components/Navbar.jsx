136e6a5fa4705592fc36c6f0e3d6fe29c86c6ea5
WasefUllah <wasefudvash@gmail.com>
2025-05-09 18:00:57 +0600

    added login logout functionality

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { use } from "react";
 import logo from "../assets/logo.png";
 import { Link, NavLink } from "react-router";
+import { AuthContext } from "../Provider/AuthProvider";
 const Navbar = () => {
+  const { userInfo, logOut } = use(AuthContext);
+  const handleLogoutBtn = () => {
+    logOut();
+  };
+  console.log(userInfo);
   return (
     <div className="w-11/12 mx-auto flex justify-between items-center py-3">
       <div>
@@ -9,11 +15,21 @@ const Navbar = () => {
       </div>
       <div className="flex justify-center items-center gap-5">
         <NavLink to="/">App</NavLink>
-        <NavLink to="/myProfile">My profile</NavLink>
+        {userInfo && <NavLink to="/myProfile">My profile</NavLink>}
       </div>
       <div>
-        <Link to={"/auth/login"} className="btn btn-primary">Login</Link>
+        {userInfo ? (
+          
+          <button className="btn btn-primary" onClick={handleLogoutBtn}>
+          Logout
+        </button>
+        ) : (
+          <Link to={"/auth/login"} className="btn btn-primary">
+            Login
+          </Link>
+        )}
       </div>
+      
     </div>
   );
 };
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,9 +1,62 @@
-import React, { createContext } from "react";
-
+import React, { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import app from "../Firebase/firebase.config";
 export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
+  const auth = getAuth(app);
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUserInfo(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
+  const logOut = () => {
+    return signOut(auth)
+      .then(() => {
+        alert("logged out");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorCode, " ", errorMessage);
+      });
+  };
+
+  const updateUser = (updatedUser) => {
+    return updateProfile(auth.currentUser, updatedUser);
+  };
+
+  const [userInfo, setUserInfo] = useState(null);
+  const [loading, setLoading] = useState(true);
   const AuthData = {
-    name: "wasef",
+    userInfo,
+    setUserInfo,
+    createUser,
+    logOut,
+    signIn,
+    loading,
+    setLoading,
+    updateUser,
   };
   return <AuthContext value={AuthData}>{children}</AuthContext>;
 };
